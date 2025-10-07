236a5b3c313ef59d5c33a46c670546f1b6d1b7c9
Osmond F <of000010@sd38.bc.ca>
2025-10-07 11:13:59 -0700

    n2

---
+++ notes-2-methods.py
@@ -0,0 +1,9 @@
+# Ask the customer if they want fies
+fries_reply = input("Do you want fries?")
+
+# "yes!"
+
+if "yes" in fries_reply.lower():
+    print("Here are your fries.")
+else:
+    print("Ok you will not have fries.")
