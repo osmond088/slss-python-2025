b804fa528165708a6c0b1850f012da516a58e092
Osmond F <of000010@sd38.bc.ca>
2025-10-09 11:40:31 -0700

    Notes 3

---
+++ notes-3-functions.py
@@ -0,0 +1,14 @@
+# function to print hello to the console
+
+
+def say_hello():
+    print("hello")
+
+
+# functions to print a personalized helo
+def say_hello_personal(name: str):
+    print(f"hello {name}!")
+
+
+say_hello_personal("osmond")
+say_hello_personal("spiderman")
